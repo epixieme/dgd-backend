60105219768851a8c62ebc5a045064751c831d97
Kirstie Hayes <kirstie_hayes@hotmail.com>
2023-11-24 11:14:11 +0000

    adding additional filters

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { v1: uuid } = require('uuid')
 
 let dogs = [
   {
-  
+
     id: "3d594650-3436-11e9-bc57-8b80ba54c431",
     name: "Wolfy",
     description: "Large dog, very friendly and ready to meet other large breeds",
@@ -13,26 +13,31 @@ let dogs = [
     likes: "Juicy Bones",
   },
   {
-  
+
     id: '3d599470-3436-11e9-bc57-8b80ba54c431',
     name: "Wolfy2",
     description: "Large dog, very friendly and ready to meet other large breeds",
     imageUrl: "https://i.ibb.co/tzWGk3j/husky.jpg",
     likes: "Juicy Bones",
   },
-  
+
   {
-  
+
     id: '3d599471-3436-11e9-bc57-8b80ba54c431',
     name: "Wolfy3",
     description: "Large dog, very friendly and ready to meet other large breeds",
     imageUrl: "https://i.ibb.co/tzWGk3j/husky.jpg",
     likes: "Juicy Bones",
   },
-  
+
 ]
 
 const typeDefs = `
+type Address {
+  street: String!
+  city: String!
+
+}
 
   type Dog {
     id: ID!
@@ -40,12 +45,17 @@ const typeDefs = `
     description: String!
     imageUrl: String!
     likes: String!
-   
+    address: Address!
   }
 
+  enum YesNo {
+    YES
+    NO
+  }
+ 
   type Query {
     dogCount: Int!
-    allDogs: [Dog!]!
+    allDogs(street: YesNo): [Dog!]!
     findDog(name: String!): Dog
   }
 
@@ -57,20 +67,35 @@ const typeDefs = `
       imageUrl: String!
       likes: String!
     ): Dog
+    editStreet (
+      name: String!
+      street: String!
+    ): Dog
   }
 `
 
 const resolvers = {
- 
+
   Query: {
     dogCount: () => dogs.length,
-    allDogs: () => dogs,
+    allDogs: (root, args) => {
+      if (!args.street)
+        return dogs
+      const byStreet = (dog) => args.street === 'yes' ? dog.street : !dog.street
+      return dogs.filter(byStreet)
+    },
     findDog: (root, args) =>
       dogs.find(d => d.name === args.name)
   },
-  // Dog: {
-  //   description:string
-  // },
+  Dog: {
+    // apollo handles the other resolvers by default if not specified like the address is below eg name, description and so on
+    address: (root) => {
+      return {
+        street: root.street,
+        city: root.city
+      }
+    }
+  },
   Mutation: {
     addDog: (root, args) => {
       if (dogs.find(d => d.name === args.name)) {
